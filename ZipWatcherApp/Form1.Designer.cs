fe42caf331e88a53fa515a4c93bf19c46ebe4d06
brada <gamyuen@hotmail.com>
2018-11-24 19:36:24 +1100

    1st commit

---
+++ ZipWatcherApp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ZipWatcherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(343, 40);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(39, 20);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(43, 87);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(145, 87);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(250, 87);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(75, 23);
+            this.btnLog.TabIndex = 3;
+            this.btnLog.Text = "See Log";
+            this.btnLog.UseVisualStyleBackColor = true;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(43, 40);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(282, 20);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 237);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ ZipWatcherApp/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+
+namespace ZipWatcherApp
+{
+    public partial class Form1 : Form
+    {
+        private Boolean isActive;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            fbd.Description = $"Choose a Folder to Watch";
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                // show the path in the text box
+                this.textBox.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            isActive = true;
+
+            FileSystemWatcher rootWatcher = new FileSystemWatcher();
+            rootWatcher.Path = textBox.Text;
+            rootWatcher.Created += new FileSystemEventHandler(rootWatcher_Created);
+            rootWatcher.EnableRaisingEvents = true;
+            rootWatcher.Filter = "*.*";
+        }
+
+        private void rootWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            string watchedPath = e.FullPath; // watch the root folder for any file creation.
+
+            var isFolder = Directory.Exists(watchedPath);
+            var isFile = File.Exists(watchedPath);
+
+            //MessageBox.Show(watchRootLv);
+
+            while (watchedPath.Contains("New folder"))
+            {
+                MessageBox.Show($"naming not accepted.");
+                return;
+            }
+            MessageBox.Show($"{e.Name} Directory : {e.ChangeType} on {DateTime.Now.ToString()} \r\n");
+
+
+            if (isFile)
+            {
+                MessageBox.Show($"{e.Name} File : {e.ChangeType} on {DateTime.Now.ToString()} \r\n");
+                return;
+            }
+
+            // create another watcher for file creation and send event to timer
+            FileSystemWatcher subFolderWatcher = new FileSystemWatcher();
+            subFolderWatcher.Path = watchedPath;
+
+            if (isActive)
+            {
+                var aTimer = new System.Timers.Timer();
+                aTimer.Interval = 15000;
+
+                // Lambda == args => expression
+                // send event to subFolderWatcher
+                aTimer.Elapsed += new ElapsedEventHandler((timerSender, timerEvt) =>
+                    OnTimedEvent(timerSender, timerEvt, subFolderWatcher));
+                aTimer.AutoReset = false;
+                aTimer.Enabled = true;
+
+                // sub-folder sends event to timer (and wait timer to notify subfolder)
+                subFolderWatcher.Created +=
+                    new FileSystemEventHandler((s, evt) => subFolderWatcher_Created(s, evt, aTimer));
+                subFolderWatcher.Filter = "*.*";
+                subFolderWatcher.EnableRaisingEvents = true;
+            }
+
+        }
+
+        private void subFolderWatcher_Created(object sender, FileSystemEventArgs evt, System.Timers.Timer aTimer)
+        {
+            // if new file created, stop the timer
+            //  then restart the timer
+            aTimer.AutoReset = false;
+            aTimer.Stop();
+            aTimer.Enabled = false;
+            aTimer.Enabled = true;
+            Console.WriteLine($"restart the timer as {evt.Name} created on {DateTime.Now.ToString()}");
+
+        }
+
+        private void OnTimedEvent(object timerSender, ElapsedEventArgs timerEvt, FileSystemWatcher subFolderWatcher)
+        {
+            subFolderWatcher.EnableRaisingEvents = false;
+
+            // Explicit Casting
+            Timer timer = timerSender as Timer;
+            if (timer != null) timer.Stop();
+            //timer.Dispose();
+
+            // Once time elapsed, zip the folder here?
+            Console.WriteLine($"time up. zip process begin at {timerEvt.SignalTime} \r\n");
+
+            var file = new FileInfo(textBox.Text);
+            var parentDir = file.Directory == null ? null : file.Directory.Parent; // test if dir or not
+
+            //if (parentDir != null)
+            //{
+            //    SevenZip zip = _zip;
+            //    zip.CreateZipFolder(subFolderWatcher.Path, subFolderWatcher.Path + ".7z");
+            //}
+
+            subFolderWatcher.Dispose();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+        }
+    }
+}
